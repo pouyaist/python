29fd87552c91c6c1067af40e33335a578eb559a4
Pouya Samadi <pspouya90@gmail.com@>
2017-09-14 10:29:38 +0200

    plot data

--- twiiter_bot.py
+++ twiiter_bot.py
@@ -32,6 +32,8 @@ def twitter_setup():
 
 
 
+number_of_twitts = 20
+
     # We create an extractor object:
 extractor = twitter_setup()
 
@@ -42,16 +44,70 @@ print("Number of tweets extracted: {}.\n".format(len(tweets)))
 # We print the most recent 5 tweets:
 
 print "*****************************"
-print "10 recent tweets:\n" 
+print str(number_of_twitts) + " recent tweets:\n" 
 print "*****************************"
-for tweet in tweets[:10]:
+for tweet in tweets[:number_of_twitts]:
     print tweet.text
     print "####################################" 
 
 # We create a pandas dataframe as follows:
 data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
 
+
+# We add relevant data:
+data['len']  = np.array([len(tweet.text) for tweet in tweets])
+data['ID']   = np.array([tweet.id for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+
+
 # We display the first 10 elements of the dataframe:
-display(data.head(10))
+display(data.head(number_of_twitts))
 
 
+# We create time series for data:
+
+tlen = pd.Series(data=data['len'].values, index=data['Date'])
+tfav = pd.Series(data=data['Likes'].values, index=data['Date'])
+tret = pd.Series(data=data['RTs'].values, index=data['Date'])
+
+# Lenghts along time:
+tlen.plot(figsize=(16,4), color='r')
+#plt.show()
+
+
+# Likes vs retweets visualization:
+tfav.plot(figsize=(16,4), label="Likes", legend=True)
+tret.plot(figsize=(16,4), label="Retweets", legend=True)
+#plt.show()
+
+
+
+# We obtain all possible sources:
+sources = []
+for source in data['Source']:
+    if source not in sources:
+        sources.append(source)
+
+# We print sources list:
+print("Creation of content sources:")
+for source in sources:
+    print("* {}".format(source))
+
+# We create a numpy vector mapped to labels:
+percent = np.zeros(len(sources))
+
+for source in data['Source']:
+    for index in range(len(sources)):
+        if source == sources[index]:
+            percent[index] += 1
+            pass
+
+percent /= 100
+
+# Pie chart:
+pie_chart = pd.Series(percent, index=sources, name='Sources')
+pie_chart.plot.pie(fontsize=11, autopct='%.2f', figsize=(6, 6));
+plt.show()
