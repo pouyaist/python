a37828aa8a0619d660fb475235730ac7125a2303
Pouya Samadi <pspouya90@gmail.com@>
2017-09-14 11:10:40 +0200

    analyze postive and negative comments

--- twiiter_bot.py
+++ twiiter_bot.py
@@ -9,11 +9,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 #matplotlib inline
 
-
-
-
 # We import our access keys:
 from credentials import *    # This will allow us to use the keys as variables
+
+from textblob import TextBlob
+import re
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean the text in a tweet by removing 
+    links and special characters using regex.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return "Positive"
+    elif analysis.sentiment.polarity == 0:
+        return "Neutral"
+    else:
+        return "Negative"
+
+
 
 # API's setup:
 def twitter_setup():
@@ -32,23 +54,28 @@ def twitter_setup():
 
 
 
+
+
+
 number_of_twitts = 20
 
-    # We create an extractor object:
+# We create an extractor object:
 extractor = twitter_setup()
 
+twitter_account_name = raw_input("Please enter twitter account name to Analyze:")
+
+
 # We create a tweet list as follows:
-tweets = extractor.user_timeline(screen_name="realDonaldTrump", count=200)
+tweets = extractor.user_timeline(screen_name=twitter_account_name, count=200)
 print("Number of tweets extracted: {}.\n".format(len(tweets)))
 
 # We print the most recent 5 tweets:
-
-print "*****************************"
+print "*****************************************************************************"
 print str(number_of_twitts) + " recent tweets:\n" 
-print "*****************************"
+print "*****************************************************************************"
 for tweet in tweets[:number_of_twitts]:
     print tweet.text
-    print "####################################" 
+    print "------------------------------------------------------------" 
 
 # We create a pandas dataframe as follows:
 data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
@@ -56,13 +83,14 @@ data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
 
 # We add relevant data:
 data['len']  = np.array([len(tweet.text) for tweet in tweets])
-data['ID']   = np.array([tweet.id for tweet in tweets])
+#data['ID']   = np.array([tweet.id for tweet in tweets])
 data['Date'] = np.array([tweet.created_at for tweet in tweets])
 data['Source'] = np.array([tweet.source for tweet in tweets])
 data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
 data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+data['SA'] = np.array([ analize_sentiment(tweet) for tweet in data['Tweets'] ])
 
-
+print "*****************************************************************************"
 # We display the first 10 elements of the dataframe:
 display(data.head(number_of_twitts))
 
@@ -91,6 +119,8 @@ for source in data['Source']:
     if source not in sources:
         sources.append(source)
 
+
+print "*****************************************************************************"
 # We print sources list:
 print("Creation of content sources:")
 for source in sources:
@@ -110,4 +140,20 @@ percent /= 100
 # Pie chart:
 pie_chart = pd.Series(percent, index=sources, name='Sources')
 pie_chart.plot.pie(fontsize=11, autopct='%.2f', figsize=(6, 6));
-plt.show()
+#plt.show()
+
+
+
+
+
+# We construct lists with classified tweets:
+
+pos_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == "Positive"]
+neu_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == "Neutral"]
+neg_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == "Negative"]
+
+# We print percentages:
+print "*****************************************************************************"
+print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['Tweets'])))
+print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data['Tweets'])))
+print("Percentage de negative tweets: {}%".format(len(neg_tweets)*100/len(data['Tweets'])))	
