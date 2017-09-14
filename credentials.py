f891ccbfc7d8b4101dac14640734a2b3d46fc4b8
Pouya Samadi <pspouya90@gmail.com@>
2017-09-14 10:14:32 +0200

    extract random twitts

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'ZpTwgO1RU56vFGe4qVcsBi1TX'
+CONSUMER_SECRET = 'b4a89nbWdnUc0RPatuaNh4rXEeUUAghXiR13SjhRfDEJbxqjA5'
+
+# Access:
+ACCESS_TOKEN  = '719220110838267904-SSlN302M7NxeLw55qigAfNf27H3fQ9T'
+ACCESS_SECRET = '1nAh05QtjvLTitsqhKSVt9BrdAEsCFBLV1rkggTcGhjzR'
---
+++ twiiter_bot.py
@@ -0,0 +1,57 @@
+# General:
+import tweepy           # To consume Twitter's API
+import pandas as pd     # To handle data
+import numpy as np      # For number computing
+
+# For plotting and visualization:
+from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+#matplotlib inline
+
+
+
+
+# We import our access keys:
+from credentials import *    # This will allow us to use the keys as variables
+
+# API's setup:
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with our access keys provided.
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+
+
+
+
+    # We create an extractor object:
+extractor = twitter_setup()
+
+# We create a tweet list as follows:
+tweets = extractor.user_timeline(screen_name="realDonaldTrump", count=200)
+print("Number of tweets extracted: {}.\n".format(len(tweets)))
+
+# We print the most recent 5 tweets:
+
+print "*****************************"
+print "10 recent tweets:\n" 
+print "*****************************"
+for tweet in tweets[:10]:
+    print tweet.text
+    print "####################################" 
+
+# We create a pandas dataframe as follows:
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+# We display the first 10 elements of the dataframe:
+display(data.head(10))
+
+
